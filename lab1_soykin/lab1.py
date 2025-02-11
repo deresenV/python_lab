e2ff5a5fbb1cd237c9235944ffd4fa53bc98a2c6
deresen <deresenvil@gmail.com>
2025-02-11 19:28:43 +0300

    fix logger B + add command do

--- lab1_soykin/lab1.py
+++ lab1_soykin/lab1.py
@@ -7,12 +7,8 @@ def check_pos(x, y, danger_zones):
         print("Дальше проход невозможен!\nЗапретная зона!")
         return False
     return True
-
-
 def move(rotation, steps, x, y, log_moves, danger_zones, logger=True):
-    if logger:
-        log_moves.insert(0, (rotation, steps))
-
+    move_count=0
     for _ in range(steps):
         new_x, new_y = x, y
         if rotation == 'R':
@@ -26,9 +22,13 @@ def move(rotation, steps, x, y, log_moves, danger_zones, logger=True):
 
         if check_pos(new_x, new_y, danger_zones):
             x, y = new_x, new_y
+            move_count += 1
             print(x, y)
         else:
             break
+    if logger:
+        log_moves.insert(0, (rotation, move_count))
+
 
     return x, y, log_moves
 
@@ -69,23 +69,27 @@ def main_game():
     x, y = 1, 1
     log_moves = []
     danger_zones = []
-
+    commands=[]
     while True:
-        command = input().split(',')
+        input_command=input().split(',')
+        if input_command[0]=='do':
+            for command in commands:
+                if command[0] == 'B':
+                    steps_back = int(command[1]) if len(command) > 1 else 1
+                    x, y, log_moves = back(log_moves, x, y, danger_zones, steps_back)
 
-        if command[0] == 'B':
-            steps_back = int(command[1]) if len(command) > 1 else 1
-            x, y, log_moves = back(log_moves, x, y, danger_zones, steps_back)
+                elif len(command) == 2:
+                    rotation, steps = command[0], int(command[1])
+                    x, y, log_moves = move(rotation, steps, x, y, log_moves, danger_zones)
 
-        elif len(command) == 2:
-            rotation, steps = command[0], int(command[1])
-            x, y, log_moves = move(rotation, steps, x, y, log_moves, danger_zones)
+                elif len(command) == 4:
+                    danger_zones = zones(command)
 
-        elif len(command) == 4:
-            danger_zones = zones(command)
-
+                else:
+                    print("Некорректная команда!")
+            commands=[]
         else:
-            print("Некорректная команда!")
+            commands.append(input_command)
 
 
 
