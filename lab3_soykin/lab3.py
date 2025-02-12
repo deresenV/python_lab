20edcb39fce66d84e264b75136c9c8f38503f373
deresenV <deresenvil@gmail.com>
2025-02-12 10:11:28 +0300

    init

---
+++ lab3_soykin/lab3.py
@@ -0,0 +1,14 @@
+import statistics
+import csv
+import sys
+
+from split_data import split_data
+
+def read_data_from_file():
+    pass
+def calculate_statistics():
+    pass
+def main():
+    if len(sys.argv) < 3:
+        print("python3 lab3.py file_path interval")
+        sys.exit(1)
---
+++ lab3_soykin/split_data.py
@@ -0,0 +1,4 @@
+from lab3 import *
+
+def split_data():
+    pass
